7a8e6784c27e090d882d68fada9730aba4d541bf
Indraneel Ganguli <indraneel_in@yahoo.com>
2026-03-29 16:40:55 +0530

    working copy

--- detectSound_alert/detectSound_alert.ino
+++ detectSound_alert/detectSound_alert.ino
@@ -1,16 +1,28 @@
 //This code is for Arduino Nano and detects Sound and buzzes alarm
 
-const int soundPinAnalog = A2 ;
-int soundLevel = 0 ;
+const int soundPinDigital = 2;
+const int buzzerPinDigital = 4;
+int soundLevel;
+bool soundDetected;
 void setup() {
   Serial.begin(9600);
-  pinMode(soundPinAnalog,INPUT);
+  pinMode(soundPinDigital, INPUT);
+  pinMode(buzzerPinDigital,OUTPUT);
   Serial.println("Starting program");
+  attachInterrupt(digitalPinToInterrupt(soundPinDigital), soundISR, FALLING);
 }
 
 void loop() {
-  
-  soundLevel = analogRead(soundPinAnalog);
-  Serial.println("value read "+soundLevel);
-  
+  if (soundDetected){
+    soundDetected=false;
+    tone(buzzerPinDigital,3000);
+    delay(200);
+    noTone(buzzerPinDigital);
+
+  }
+}
+
+void soundISR() {
+  Serial.println("Inside soundISR");
+  soundDetected = true;
 }
